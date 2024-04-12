2d24b375bbc4b9af5f295b4763dcaf9e3c6018ff
Tailan Souza <tailansouza.dev@gmail.com>
2024-04-11 22:07:21 -0300

    ajustes

--- js/section-produtos-verMais.js
+++ js/section-produtos-verMais.js
@@ -1,16 +1,28 @@
+// Selecionar os cartões e o botão "Ver Mais"
 var cartoes = document.querySelectorAll('.produtos-cads .cards-4, .produtos-cads .cards-5, .produtos-cads .cards-6');
 var btnVerMais = document.querySelector('#btn-ver-mais');
 
+
+cartoes.forEach(function(cartao) {
+    cartao.style.display = 'none';
+});
+
+
 var cartoesExibidos = false;
 
+// Função para exibir e ocultar os cartões
 function exibirCartoes() {
     if (!cartoesExibidos) {
+        // Exibir os cartões
         cartoes.forEach(function(cartao) {
             cartao.style.display = 'block';
+            cartao.style.margin = 'auto'; 
+            cartao.style.display = 'flex'; // esta em teste
         });
         btnVerMais.innerText = 'Ver Menos';
         cartoesExibidos = true;
     } else {
+        // Ocultar os cartões
         cartoes.forEach(function(cartao) {
             cartao.style.display = 'none';
         });
@@ -18,5 +30,6 @@ function exibirCartoes() {
         cartoesExibidos = false;
     }
 }
+
 
 btnVerMais.addEventListener('click', exibirCartoes);
